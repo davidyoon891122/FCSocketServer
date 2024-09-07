36aee9b598bdec2b4483adffa14882f18e327b77
David Yoon <qqpo12@naver.com>
2024-09-07 20:34:14 +0900

    Add the OKHTTP
    * Added the okHTTP to request network service

--- app/src/main/java/com/example/fcsocketserver/MainActivity.kt
+++ app/src/main/java/com/example/fcsocketserver/MainActivity.kt
@@ -6,7 +6,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.HttpURLConnection
@@ -20,34 +26,58 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val client = OkHttpClient()
 
-        Thread {
-            try {
-                val socket = Socket("10.0.2.2", 8080)
-                val printer = PrintWriter(socket.getOutputStream())
-                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val request: Request = Request.Builder()
+            .url("http://10.0.2.2:8080")
+            .build()
 
-
-                printer.println("GET / HTTP/1.1")
-                printer.println("Host: 127.0.0.1:8080")
-                printer.println("User-Agent: android")
-                printer.println("\r\n")
-                printer.flush()
-
-                var input: String? = "-1"
-                while(input != null) {
-                    input = reader.readLine()
-                    Log.e("Client", input)
-                }
-
-                reader.close()
-                printer.close()
-                socket.close()
-            } catch (e: Exception) {
+        val callBack = object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
                 Log.e("Client", e.toString())
             }
 
-        }.start()
+            override fun onResponse(call: Call, response: Response) {
+
+                if(response.isSuccessful) {
+                    Log.e("Client", "${response.body?.string()}")
+                }
+            }
+        }
+
+        client.newCall(request).enqueue(callBack)
+
+
+
+
+        // 소켓으로 네트워크 처리
+//        Thread {
+//            try {
+//                val socket = Socket("10.0.2.2", 8080)
+//                val printer = PrintWriter(socket.getOutputStream())
+//                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+//
+//
+//                printer.println("GET / HTTP/1.1")
+//                printer.println("Host: 127.0.0.1:8080")
+//                printer.println("User-Agent: android")
+//                printer.println("\r\n")
+//                printer.flush()
+//
+//                var input: String? = "-1"
+//                while(input != null) {
+//                    input = reader.readLine()
+//                    Log.e("Client", input)
+//                }
+//
+//                reader.close()
+//                printer.close()
+//                socket.close()
+//            } catch (e: Exception) {
+//                Log.e("Client", e.toString())
+//            }
+//
+//        }.start()
 
 
     }
